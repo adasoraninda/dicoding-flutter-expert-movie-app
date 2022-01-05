4c5d01383c4925d5596514770828cb80c4059242
adasoraninda <adasoraninda@gmail.com>
2022-01-05 14:05:26 +0700

    Add some test

--- search/test/presentation/bloc/search_bloc_test.dart
+++ search/test/presentation/bloc/search_bloc_test.dart
@@ -1,6 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:core/domain/entities/movies/movie.dart';
 import 'package:core/utils/failure.dart';
+import 'package:core/utils/film_type_enum.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -12,6 +12,7 @@ import 'package:search/presentation/bloc/search/search_event.dart';
 import 'package:search/presentation/bloc/search/search_state.dart';
 
 import '../../dummy_data/dummy_objects.dart';
+import 'search_bloc_test.mocks.dart';
 
 @GenerateMocks([SearchMovies, SearchTvShows])
 void main() {
@@ -19,86 +20,255 @@ void main() {
   late MockSearchMovies mockSearchMovies;
   late MockSearchTvShows mockSearchTvShows;
 
-  group('SearchMovies', () {
-    setUp(() {
-      mockSearchMovies = MockSearchMovies();
-      mockSearchTvShows = MockSearchTvShows();
+  setUp(() {
+    mockSearchMovies = MockSearchMovies();
+    mockSearchTvShows = MockSearchTvShows();
 
-      searchBloc = SearchBloc(
-        mockSearchMovies,
-        mockSearchTvShows,
-      );
-    });
+    searchBloc = SearchBloc(
+      mockSearchMovies,
+      mockSearchTvShows,
+    );
+  });
 
-    test('initial state should be empty', () {
-      expect(searchBloc.state, SearchEmpty());
-    });
+  test('initial state should be empty', () {
+    expect(searchBloc.state, SearchState.init());
+    expect(searchBloc.state.tvShows, []);
+    expect(searchBloc.state.movies, []);
+  });
 
-    group('OnQueryChange', () {
-      blocTest<SearchBloc, SearchState>(
-        'Should emit [Loading, HasData] when data is gotten successfully',
-        build: () {
-          when(mockSearchMovies.execute(tQuery))
-              .thenAnswer((_) async => Right(tMovieList));
+  group('OnQueryChange', () {
+    blocTest<SearchBloc, SearchState>(
+      '[Type Movie] Should emit state [init, loading, result] when data is gotten successfully',
+      build: () {
+        when(mockSearchMovies.execute(tQuery))
+            .thenAnswer((_) async => Right(tMovieList));
 
-          return searchBloc;
-        },
-        act: (bloc) => bloc.add(const OnQueryChanged(tQuery)),
-        wait: const Duration(milliseconds: 500),
-        expect: () => [
-          SearchLoading(),
-          SearchHasData<Movie>(tMovieList),
-        ],
-        verify: (bloc) {
-          verify(mockSearchMovies.execute(tQuery));
-        },
-      );
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchMovieTypeEvent());
+        bloc.add(const OnQueryChanged(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.movies, [], [], false, false, null, null),
+        const SearchState(FilmType.movies, [], [], true, false, null, null),
+        SearchState(
+            FilmType.movies, tMovieList, const [], false, false, null, null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchMovies);
+        verifyZeroInteractions(mockSearchTvShows);
+      },
+    );
 
-      blocTest<SearchBloc, SearchState>(
-        'Should emit [Loading, Error] when get search is unsuccessful',
-        build: () {
-          when(mockSearchMovies.execute(tQuery)).thenAnswer(
-              (_) async => const Left(ServerFailure('Server Failure')));
+    blocTest<SearchBloc, SearchState>(
+      '[Type Movie] Should emit state [init, loading, failure] when get search is unsuccessful',
+      build: () {
+        when(mockSearchMovies.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
 
-          return searchBloc;
-        },
-        act: (bloc) {
-          bloc.add(const OnQueryChanged(tQuery));
-        },
-        wait: const Duration(milliseconds: 500),
-        expect: () => [
-          SearchLoading(),
-          const SearchError('Server Failure'),
-        ],
-        verify: (bloc) {
-          verify(mockSearchMovies.execute(tQuery));
-        },
-      );
-    });
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchMovieTypeEvent());
+        bloc.add(const OnQueryChanged(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.movies, [], [], false, false, null, null),
+        const SearchState(FilmType.movies, [], [], true, false, null, null),
+        const SearchState(
+            FilmType.movies, [], [], false, false, 'Server Failure', null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchMovies);
+        verifyZeroInteractions(mockSearchTvShows);
+      },
+    );
 
-    group('OnQuerySubmit', () {});
+    blocTest<SearchBloc, SearchState>(
+      '[Type Tv] Should emit state [init, loading, result] when data is gotten successfully',
+      build: () {
+        when(mockSearchTvShows.execute(tQuery))
+            .thenAnswer((_) async => Right(tTvShowList));
 
-    group('SearchMovieEvent', () {
-      blocTest<SearchBloc, SearchState>(
-        'Should emit [Loading, HasData] when data is gotten successfully',
-        build: () {
-          when(mockSearchMovies.execute(tQuery))
-              .thenAnswer((_) async => Right(tMovieList));
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchTvShowTypeEvent());
+        bloc.add(const OnQueryChanged(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.tvShows, [], [], false, false, null, null),
+        const SearchState(FilmType.tvShows, [], [], false, true, null, null),
+        SearchState(
+            FilmType.tvShows, const [], tTvShowList, false, false, null, null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTvShows.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchTvShows);
+        verifyZeroInteractions(mockSearchMovies);
+      },
+    );
 
-          return searchBloc;
-        },
-        act: (bloc) {
-          bloc.add(const OnQueryChanged(tQuery));
-          bloc.add(SearchMovieEvent());
-        },
-        expect: () => [
-          SearchLoading(),
-          SearchHasData<Movie>(tMovieList),
-        ],
-        verify: (bloc) {
-          verify(mockSearchMovies.execute(tQuery));
-        },
-      );
-    });
+    blocTest<SearchBloc, SearchState>(
+      '[Type Tv] Should emit state [init, loading, failure] when get search is unsuccessful',
+      build: () {
+        when(mockSearchTvShows.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
+
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchTvShowTypeEvent());
+        bloc.add(const OnQueryChanged(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.tvShows, [], [], false, false, null, null),
+        const SearchState(FilmType.tvShows, [], [], false, true, null, null),
+        const SearchState(
+            FilmType.tvShows, [], [], false, false, null, 'Server Failure'),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTvShows.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchTvShows);
+        verifyZeroInteractions(mockSearchMovies);
+      },
+    );
+  });
+
+  group('OnQuerySubmit', () {
+    blocTest<SearchBloc, SearchState>(
+      '[Type Movie] Should emit state [init, loading, result] when data is gotten successfully',
+      build: () {
+        when(mockSearchMovies.execute(tQuery))
+            .thenAnswer((_) async => Right(tMovieList));
+
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchMovieTypeEvent());
+        bloc.add(const OnQuerySubmit(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.movies, [], [], false, false, null, null),
+        const SearchState(FilmType.movies, [], [], true, false, null, null),
+        SearchState(
+            FilmType.movies, tMovieList, const [], false, false, null, null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchMovies);
+        verifyZeroInteractions(mockSearchTvShows);
+      },
+    );
+
+    blocTest<SearchBloc, SearchState>(
+      '[Type Movie] Should emit state [init, loading, failure] when get search is unsuccessful',
+      build: () {
+        when(mockSearchMovies.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
+
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchMovieTypeEvent());
+        bloc.add(const OnQuerySubmit(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.movies, [], [], false, false, null, null),
+        const SearchState(FilmType.movies, [], [], true, false, null, null),
+        const SearchState(
+            FilmType.movies, [], [], false, false, 'Server Failure', null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchMovies);
+        verifyZeroInteractions(mockSearchTvShows);
+      },
+    );
+
+    blocTest<SearchBloc, SearchState>(
+      '[Type Tv] Should emit state [init, loading, result] when data is gotten successfully',
+      build: () {
+        when(mockSearchTvShows.execute(tQuery))
+            .thenAnswer((_) async => Right(tTvShowList));
+
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchTvShowTypeEvent());
+        bloc.add(const OnQuerySubmit(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.tvShows, [], [], false, false, null, null),
+        const SearchState(FilmType.tvShows, [], [], false, true, null, null),
+        SearchState(
+            FilmType.tvShows, const [], tTvShowList, false, false, null, null),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTvShows.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchTvShows);
+        verifyZeroInteractions(mockSearchMovies);
+      },
+    );
+
+    blocTest<SearchBloc, SearchState>(
+      '[Type Tv] Should emit state [init, loading, failure] when get search is unsuccessful',
+      build: () {
+        when(mockSearchTvShows.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
+
+        return searchBloc;
+      },
+      act: (bloc) {
+        bloc.add(SearchTvShowTypeEvent());
+        bloc.add(const OnQuerySubmit(tQuery));
+      },
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        const SearchState(FilmType.tvShows, [], [], false, false, null, null),
+        const SearchState(FilmType.tvShows, [], [], false, true, null, null),
+        const SearchState(
+            FilmType.tvShows, [], [], false, false, null, 'Server Failure'),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTvShows.execute(tQuery));
+        verifyNoMoreInteractions(mockSearchTvShows);
+        verifyZeroInteractions(mockSearchMovies);
+      },
+    );
+  });
+
+  group('SearchTypeEvent', () {
+    blocTest<SearchBloc, SearchState>(
+      '[Type Tv] Should emit state FilmType tvShows',
+      build: () => searchBloc,
+      act: (bloc) {
+        bloc.add(SearchTvShowTypeEvent());
+      },
+      expect: () => [
+        const SearchState(FilmType.tvShows, [], [], false, false, null, null),
+      ],
+    );
+
+    blocTest<SearchBloc, SearchState>(
+      '[Type Movie] Should emit state FilmType movies',
+      build: () => searchBloc,
+      act: (bloc) {
+        bloc.add(SearchMovieTypeEvent());
+      },
+      expect: () => [
+        const SearchState(FilmType.movies, [], [], false, false, null, null),
+      ],
+    );
   });
 }
---
+++ search/test/presentation/pages/search_page_test.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:search/presentation/bloc/search/search_bloc.dart';
+
+@GenerateMocks([SearchBloc])
+void main() {}
